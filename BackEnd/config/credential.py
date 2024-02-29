80864d0a3706cba7df21df35c78f444b6d1b65c8
Finn111 <60770307+Finn111@users.noreply.github.com>
2024-02-28 20:05:42 -0400

    adding credential

---
+++ BackEnd/config/credential.py
@@ -0,0 +1,7 @@
+prjobs_config = {
+    'host': 'ec2-34-206-106-3.compute-1.amazonaws.com',
+    'user': 'fhyvdcoaybhwvo',
+    'password': 'b4848d570fef2cabfe2dc6f198f22b007fab3d9d688cbb446945e10f4132e239',
+    'name': 'dfts6ob5te25ml',
+    'port': 5432,
+}
---
+++ BackEnd/config/setup.py
@@ -0,0 +1,54 @@
+from config.credential import prjobs_config
+import psycopg2
+
+
+class SetupTables:
+    def __init__(self):
+        self.sql_commands = None
+        connection_url = "name=%s user=%s password=%s port=%s host=%s" % (
+            prjobs_config['name'],
+            prjobs_config['user'],
+            prjobs_config['password'],
+            prjobs_config['port'],
+            prjobs_config['host'])
+        self.conn = psycopg2.connect(connection_url, sslmode='require')
+
+    def loadSqlFile(self, filename: str):
+        # Open and read the file as a single buffer
+        try:
+            fd = open(filename, 'r')
+            sqlFile = fd.read()
+            fd.close()
+
+        except IOError as msg:
+            print("ERROR: ", msg)
+            return
+
+        # all SQL commands (split on ';')
+        sqlCommands = sqlFile.split(';')
+        if sqlCommands[len(sqlCommands)-1] == '':
+            sqlCommands.pop()
+        print("THE LAST  ", sqlCommands[len(sqlCommands)-1])
+        self.sql_commands = sqlCommands
+        print("Commands successfully loaded")
+
+    def create(self):
+        if self.sql_commands is not None:
+
+            # Execute every command from the input file
+            cursor = self.conn.cursor()
+            for command in self.sql_commands:
+                # This will skip and report errors
+                # For example, if the tables do not yet exist, this will skip over
+                # the DROP TABLE commands
+                try:
+                    print("COMMAND: ", command)
+                    print("----------------------------")
+                    cursor.execute(command)
+                    self.conn.commit()
+                except psycopg2.OperationalError as msg:
+                    print("Command skipped: ", msg)
+                    return "FAILURE"
+            return "SUCCESS"
+        else:
+            return 'Cannot create table, no commands!'
