e733bfd503f7d9c8b575ed42bca110c83aa4d7bb
Finn111 <60770307+Finn111@users.noreply.github.com>
2024-03-01 22:13:28 -0400

    Adding Backend for Messages and searchers

---
+++ BackEnd/handlers/messages.py
@@ -0,0 +1,140 @@
+from flask import jsonify
+from dao.messages import MessagesDAO
+
+
+class MessagesHandler:
+    def build_messages_dict(self, row):
+        result = {}
+        result['msg_id'] = row[0]
+        result['user_id1'] = row[1]
+        result['user_id2'] = row[2]
+        result['msg_content'] = row[3]
+        result['msg_time'] = row[4]
+        return result
+
+
+    def build_messages_attributes(self, msg_id, user_id1, user_id2, msg_content, msg_time):
+        result = {}
+        result['msg_id'] = msg_id
+        result['user_id1'] = user_id1
+        result['user_id2'] = user_id2
+        result['msg_content'] = msg_content
+        result['msg_time'] = msg_time
+        return result
+
+    def getAllMessages(self):
+        dao = MessagesDAO()
+        message_list = dao.getAllMessages()
+        result_list = []
+        for row in message_list:
+            result = self.build_messages_dict(row)
+            result_list.append(result)
+        return jsonify({"Messages" : result_list}), 200
+
+    def getMessagesById(self, msg_id):
+        dao = MessagesDAO()
+        row = dao.getMessagesById(msg_id) 
+        if not row:
+            return jsonify(Error = "Message Not Found"), 404
+        else:
+            message = self.build_messages_dict(row)
+            return jsonify({"Message" : message}), 200
+        
+    def getMessagesbySender(self, user_id1):
+        dao = MessagesDAO()
+        row = dao.getMessagesbySender(user_id1)
+        if not row:
+            return jsonify(Error="Message Not Found"), 404
+        else:
+            message = self.build_messages_dict(row)
+            return jsonify(Message=message)
+        
+    def getMessagesbyContent(self, msg_content):
+        dao = MessagesDAO()
+        row = dao.getMessagesbyContent(msg_content)
+        if not row:
+            return jsonify(Error="Message Not Found"), 404
+        else:
+            message = self.build_messages_dict(row)
+            return jsonify(Message=message)
+        
+    def getMessagesbyTime(self, msg_time):
+        dao = MessagesDAO()
+        row = dao.getMessagesbyTime(msg_time)
+        if not row:
+            return jsonify(Error="Message Not Found"), 404
+        else:
+            message = self.build_messages_dict(row)
+            return jsonify(Message=message)
+    
+    # def searchMessages(self, args):
+    #     sender = args.get("user_id1")
+    #     dao = MessagesDAO()
+    #     messages_list = []
+    #     if (len(args) == 1) and sender:
+    #         messages_list = dao.getMessagesbySender(sender)
+    #     else:
+    #         return jsonify(Error = "Malformed query string"), 400
+    #     result_list = []
+    #     for row in messages_list:
+    #         result = self.build_messages_dict(row)
+    #         result_list.append(result)
+    #     return jsonify(Message=result_list)
+
+    def insertMessage(self, form):
+        print("form: ", form)
+        if len(form) != 4:
+            return jsonify(Error = "Malformed post request"), 400
+        else:
+            user_id1 = form['user_id1']
+            user_id2 = form['user_id2']
+            msg_content = form['msg_content']
+            msg_time = form['msg_time']
+            if user_id1 and user_id2 and msg_content and msg_time:
+                dao = MessagesDAO()
+                msg_id = dao.insert(user_id1, user_id2, msg_content, msg_time)
+                result = self.build_messages_attributes(msg_id, user_id1, user_id2, msg_content, msg_time)
+                return jsonify(Message=result), 201
+            else:
+                return jsonify(Error="Unexpected attributes in post request"), 400
+
+    def insertMessageJson(self, json):
+        user_id1 = json['user_id1']
+        user_id2 = json['user_id2']
+        msg_content = json['msg_content']
+        msg_time = json['msg_time']
+        if user_id1 and user_id2 and msg_content and msg_time:
+            dao = MessagesDAO()
+            msg_id = dao.insert(user_id1, user_id2, msg_content, msg_time)
+            result = self.build_messages_attributes(msg_id, user_id1, user_id2, msg_content, msg_time)
+            return jsonify(Message=result), 201
+        else:
+            return jsonify(Error="Unexpected attributes in post request"), 400
+
+    def deletePart(self, msg_id):
+        dao = MessagesDAO()
+        if not dao.getMessagesById(msg_id):
+            return jsonify(Error = "Message not found."), 404
+        else:
+            dao.delete(msg_id)
+            return jsonify(DeleteStatus = "OK"), 200
+
+    def updatePart(self, msg_id, form):
+        dao = MessagesDAO()
+        if not dao.getMessagesById(msg_id):
+            return jsonify(Error = "Message not found."), 404
+        else:
+            if len(form) != 4:
+                return jsonify(Error="Malformed update request"), 400
+            else:
+                user_id1 = form['user_id1']
+                user_id2 = form['user_id2']
+                msg_content = form['msg_content']
+                msg_time = form['msg_time']
+                if user_id1 and user_id2 and msg_content and msg_time:
+                    dao = MessagesDAO()
+                    msg_id = dao.insert(user_id1, user_id2, msg_content, msg_time)
+                    result = self.build_messages_attributes(msg_id, user_id1, user_id2, msg_content, msg_time)
+                    return jsonify(Message=result), 200
+                else:
+                    return jsonify(Error="Unexpected attributes in update request"), 400
