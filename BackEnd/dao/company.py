e2a201ba23da447d0fba2165128ba17ff78544be
alainadelmoral <alaina.delmoral@upr.edu>
2024-03-04 18:48:52 -0400

    fixed small error

--- BackEnd/dao/company.py
+++ BackEnd/dao/company.py
@@ -4,11 +4,11 @@ import psycopg2
 class CompanyDAO:
 
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s port=%s host=%s" % (
-            prjobs_config['dbname'],
+        connection_url = "name=%s user=%s password=%s port=%s host=%s" % (
+            prjobs_config['name'],
             prjobs_config['user'],
             prjobs_config['password'],
-            prjobs_config['dbport'],
+            prjobs_config['port'],
             prjobs_config['host'])
         self.conn = psycopg2.connect(connection_url)
 
