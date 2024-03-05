64e952d1848eb7dc2f28c4a4b4b2d610bd4e1b26
Diego-Cintron <diego.cintron1@upr.edu>
2024-03-04 20:43:01 -0400

    Fixing merge issues

--- BackEnd/dao/searchers.py
+++ BackEnd/dao/searchers.py
@@ -1,7 +1,8 @@
 from config.credential import prjobs_config
 import psycopg2
 
-class SearchersDAO:
+class PosterDAO:
+
     def __init__(self):
         connection_url = "dbname=%s user=%s password=%s port=%s host=%s" % (
             prjobs_config['name'],
@@ -11,59 +12,61 @@ class SearchersDAO:
             prjobs_config['host'])
         self.conn = psycopg2.connect(connection_url)
 
-    #Get Info on Users Job Searchers
-    def getAllSearchers(self):
+    def getAllPosters(self):
         cursor = self.conn.cursor()
-        query = "select * from searchers;"
+        query = "select * from posters;"
         cursor.execute(query)
         result = []
         for row in cursor:
             result.append(row)
         return result
     
-    def getSearchersByUserId(self, user_id):
+    # Get Poster by UserID
+    def getPosterByUserID(self, user_id):
         cursor = self.conn.cursor()
-        query = "select user_id, user_region, schedule, user_address from searchers where user_id = %s;"
+        query = "select * from posters where user_id = %s;"
         cursor.execute(query, (user_id,))
         result = cursor.fetchone()
         return result
     
-    def getSearcherbySchedule(self, schedule):
-         cursor = self.conn.cursor()
-         query = "select * from searchers where schedule = %s;"
-         cursor.execute(query, (schedule,))
-         result = []
-         for row in cursor:
-            result.append(row)
-         return result    
+    # Get Poster by Job Post ID
+    def getPostersByPostID(self, jpost_id):
+        cursor = self.conn.cursor()
+        query = "select * from posters where jpost_id = %s;"
+        cursor.execute(query, (jpost_id,))
+        result = cursor.fetchone()
+        return result
     
-    def getSearcherbyRegion(self, user_region):
-         cursor = self.conn.cursor()
-         query = "select * from searchers where user_region = %s;"
-         cursor.execute(query, (user_region,))
-         result = []
-         for row in cursor:
-            result.append(row)
-         return result    
+    # Get Poster by Company ID
+    def getPostersByCompanyID(self, cm_id):
+        cursor = self.conn.cursor()
+        query = "select * from posters where cm_id = %s;"
+        cursor.execute(query, (cm_id,))
+        result = cursor.fetchone()
+        return result
     
+    # Update Function
+    def update(self, user_id, jpost_id, cm_id):
+        cursor = self.conn.cursor()
+        query = "update posters set jpost_id = %s, cm_id = %s where user_id = %s;"
+        cursor.execute(query, (jpost_id, cm_id, user_id,))
+        self.conn.commit()
+        return user_id
+    
+    # Insert Function
+    ''' NEEDS REVISION -> regarding value being returned '''
+    def insert(self, user_id, jpost_id, cm_id):
+        cursor = self.conn.cursor()
+        query = "insert into posters(user_id, jpost_id, cm_id) values (%s, %s, %s) returning user_id;"
+        cursor.execute(query, (user_id, jpost_id, cm_id,))
+        self.conn.commit()
+        return user_id
+    
+    # Delete Function
     def delete(self, user_id):
         cursor = self.conn.cursor()
-        query = "delete from searchers where user_id = %s;"
+        query = "delete from posters where user_id = %s;"
         cursor.execute(query, (user_id,))
         self.conn.commit()
         return user_id
     
-    def update(self, user_id, user_region, schedule, user_address):
-        cursor = self.conn.cursor()
-        query = "update searchers set user_region = %s, schedule = %s, user_address = %s where user_id = %s;"
-        cursor.execute(query, (user_region, schedule, user_address,))
-        self.conn.commit()
-        return user_id
-     
-    def insert(self, user_region, schedule, user_address):
-        cursor = self.conn.cursor()
-        query = "insert into searchers(user_region, schedule, user_address) values (%s, %s, %s) returning user_id;"
-        cursor.execute(query, (user_region, schedule, user_address,))
-        user_id = cursor.fetchone()[0]
-        self.conn.commit()
-        return user_id
