82cc739b60a02c81be7f081860c82b4871e1e581
Diego-Cintron <diego.cintron1@upr.edu>
2024-03-10 10:55:47 -0400

    Made example code of fetching all the users

---
+++ FrontEnd/src/UserList.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import User from './User'; // Import the User interface
+import './index.css';
+
+const UserList: React.FC = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:5000/users');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch users');
+                }
+                const data = await response.json();
+                setUsers(data.Users); 
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    return (
+        <div>
+            <h1>User List</h1>
+            <table className="user-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user.user_id}>
+                            <td>{user.user_id}</td>
+                            <td>{user.user_fname}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default UserList;
