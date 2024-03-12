c4b6596e578e8714596677f3f35bb8a15c17fc57
Diego-Cintron <diego.cintron1@upr.edu>
2024-03-12 10:34:10 -0400

    Removed  unneeded example code

--- FrontEnd/src/UserList.tsx
+++ FrontEnd/src/UserList.tsx
@@ -1,56 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import User from './User'; // Import the User interface
-import './index.css';
+import React, { useEffect, useState } from "react";
+import User from "./User"; // Import the User interface
+import "./index.css";
 
 const UserList: React.FC = () => {
-    const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await fetch('http://127.0.0.1:5000/users');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch users');
-                }
-                const data = await response.json();
-                setUsers(data.Users);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/users");
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const data = await response.json();
+        setUsers(data.Users);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        fetchUsers();
-    }, []);
+    fetchUsers();
+  }, []);
 
-    return (
-        <div>
-            <div className="user-list">
-                <p>User List</p>
-                <table className="user-table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(person => (
-                            <tr key={person.user_id}>
-                                <td>{person.user_id}</td>
-                                <td>{person.user_fname}</td>
-                                <td>{person.user_lname}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className='secondDiv'>
-                <h1> Example 1</h1>
-                <h2> Example 2</h2>
-            </div>
-        </div>
-    );
+  return (
+    <div className="user-list">
+      <p>User List</p>
+      <table className="user-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((person) => (
+            <tr key={person.user_id}>
+              <td>{person.user_id}</td>
+              <td>{person.user_fname}</td>
+              <td>{person.user_lname}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UserList;
