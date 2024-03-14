18aa29e2d4ea822d79deead8f2c421d85ca68288
Diego-Cintron <diego.cintron1@upr.edu>
2024-03-14 14:47:07 -0400

    Created Navigation bar

---
+++ FrontEnd/src/NavigationBar.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import './NavigationBar.css';
+
+function NavigationBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavigationBar;
