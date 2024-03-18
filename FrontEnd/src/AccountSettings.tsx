3a4f3b264b9ac5d438221cc14d5834acc08931f3
Raul-Adames <raul.adames@upr.edu>
2024-03-18 15:47:15 -0400

    AccountSettings update and PUT method fixed El user id ahora es una variable que se puede cambiar data y updatedData estan definidas para evitar el warning de la pagina Los endpoint ahora se actualizan automaticamente con la varibale userId Se pueden modificar mas datos (first, last name, etc.) Funciona el PUT al darle al boton de SAVE Se crear una copia si el user_id en la funcion handleSave para realizar el PUT

--- FrontEnd/src/AccountSettings.tsx
+++ FrontEnd/src/AccountSettings.tsx
@@ -3,13 +3,40 @@ import User from "./User";
 import { errorHandle } from './apiUtils';
 
 const AccountSettings: React.FC = () => {
-  const [data, setData] = useState<User>({} as User);
-  const [updatedData, setUpdatedData] = useState<User>({} as User)
 
+  const [userId, setUserId] = useState<number>(2); // TODO: get user id dynamically
+
+  const [data, setData] = useState<User>({
+    user_id: userId,
+    user_type: "",
+    user_birthday: "",
+    user_fname: "",
+    user_lname: "",
+    user_phone: "",
+    user_email: "",
+    user_address: "",
+    user_municipality: "",
+    user_available: [""],
+  });
+
+  const [updatedData, setUpdatedData] = useState<User>({
+    user_id: userId,
+    user_type: "",
+    user_birthday: "",
+    user_fname: "",
+    user_lname: "",
+    user_phone: "",
+    user_email: "",
+    user_address: "",
+    user_municipality: "",
+    user_available: [""],
+  });
+ 
+  
   useEffect(() => {
     const fetchUser = async () => { 
         try {
-            const response = await fetch("http://127.0.0.1:5000/users/3"); // El user esta hard coded por ahora
+            const response = await fetch(`http://127.0.0.1:5000/users/${userId}`);
             errorHandle(response);
             const data = await response.json();
             setData(data.User);
@@ -20,7 +47,7 @@ const AccountSettings: React.FC = () => {
     };
     
     fetchUser();
-  }, []);
+  }, [userId]);
 
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,48 +58,52 @@ const AccountSettings: React.FC = () => {
   }
 
 
-  const handleSave = async ()  => {
+  const handleSave = async () => {
     try {
-        const response = await fetch("http://127.0.0.1:5000/users/3", {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(updatedData)
-        })
-        errorHandle(response);
-        const data = await response.json();
-        setData(data.User);
-        setUpdatedData(data.User);
+      //Crea una copia de updatedData sin el user_id
+      const { user_id, ...dataToSend } = updatedData;
+      const response = await fetch(`http://127.0.0.1:5000/users/${userId}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(dataToSend),
+      });
+      errorHandle(response);
+      const data = await response.json();
+      setData(data.User);
+      setUpdatedData(data.User);
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-  }
+  };
+
 
   return (
     <div className="user-settings">
       <h2>Account Settings</h2>
       
       <label>Email address</label>
-      <input type="email" name="user_email" value={updatedData.user_email || ""} onChange={handleChange} />
+      <input type="email" name="user_email" value={updatedData.user_email} onChange={handleChange} />
       
       <label>First name</label>
-      <input type="text" name="user_fname" value={data.user_fname} readOnly />
+      <input type="text" name="user_fname" value={updatedData.user_fname} onChange={handleChange} />
       
       <label>Last name</label>
-      <input type="text" value={data.user_lname} readOnly />
+      <input type="text" name="user_lname" value={updatedData.user_lname} onChange={handleChange} />
       
       <label>Birthday</label>
-      <input type="datetime" value={data.user_birthday} readOnly />
+      <input type="datetime" name="user_birthday" value={updatedData.user_birthday} onChange={handleChange} />
       
       <label>Phone</label>
-      <input type="tel" value={data.user_phone} readOnly />
+      <input type="tel" name="user_phone" value={updatedData.user_phone} onChange={handleChange} />
       
       <label>Address</label>
-      <input type="text" value={data.user_address} readOnly />
+      <input type="text" name="user_address" value={updatedData.user_address} onChange={handleChange} />
 
       <button onClick={handleSave}>Save</button>
 
     </div>
   );
 };
+
 
 export default AccountSettings;
