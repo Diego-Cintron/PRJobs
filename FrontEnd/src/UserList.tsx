670e2959e426053c75097ebd664492c66d16f83a
Diego-Cintron <diego.cintron1@upr.edu>
2024-03-25 20:31:26 -0400

    Moved "Posting" and "User" to Interface folder

--- FrontEnd/src/UserList.tsx
+++ FrontEnd/src/UserList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import User from "./User"; // Import the User interface
+import User from "./Interfaces/User"; // Import the User interface
 import AccountSettings from "./AccountSettings";
 import { errorHandler } from './apiUtils';
 import "./index.css";
