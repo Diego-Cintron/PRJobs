9745d7699bb09bc5df97a3adfd2d458a08a8f1b3
Finn111 <60770307+Finn111@users.noreply.github.com>
2024-04-01 18:43:25 -0400

    adding font for welcome page

--- FrontEnd/src/Welcome.tsx
+++ FrontEnd/src/Welcome.tsx
@@ -2,13 +2,11 @@ import React from "react";
 // import { errorHandle } from './apiUtils';
 import "./index.css";
 
-const Welcome: React.FC = () =>{
-    return(
-        <div>
-            
-        </div>
-    );
-
-    
-}
+const Welcome: React.FC = () => {
+  return (
+    <div className="welcome-background">
+      <div className="welcome-box">Welcome PRJobs</div>
+    </div>
+  );
+};
 export default Welcome;
