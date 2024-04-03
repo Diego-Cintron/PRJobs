78ff9e3a0464ea4cbbccdbd05fcddb46092a8b12
Diego-Cintron <diego.cintron1@upr.edu>
2024-04-03 19:22:03 -0400

    Updated credentials

--- BackEnd/config/credential.py
+++ BackEnd/config/credential.py
@@ -1,7 +1,7 @@
 prjobs_config = {
-    'host': 'ec2-34-206-106-3.compute-1.amazonaws.com',
-    'user': 'fhyvdcoaybhwvo',
-    'password': 'b4848d570fef2cabfe2dc6f198f22b007fab3d9d688cbb446945e10f4132e239',
-    'name': 'dfts6ob5te25ml',
+    'host': 'ec2-35-169-43-171.compute-1.amazonaws.com',
+    'user': 'pvdbedqypjstbz',
+    'password': 'f293bbb8fc23098407dc7607e408f222424482478bda29d4e594b006db1e4282',
+    'name': 'd1fp8e53640nn3',
     'port': 5432,
 }
