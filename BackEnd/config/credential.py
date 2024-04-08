cc78a3b8ff4eefbfd382ed10f648d6dd215180da
Diego-Cintron <diego.cintron1@upr.edu>
2024-04-08 10:39:08 -0400

    Installed serve dependency and updated credentials

--- BackEnd/config/credential.py
+++ BackEnd/config/credential.py
@@ -1,7 +1,7 @@
 prjobs_config = {
-    'host': 'ec2-35-169-43-171.compute-1.amazonaws.com',
-    'user': 'pvdbedqypjstbz',
-    'password': 'f293bbb8fc23098407dc7607e408f222424482478bda29d4e594b006db1e4282',
-    'name': 'd1fp8e53640nn3',
+    'host': 'ec2-34-230-120-83.compute-1.amazonaws.com',
+    'user': 'bsvafrctukyudy',
+    'password': 'f0fec16d599711ff6d901e4b09f85ea88ef800b8aaf1f5a56f330097d0ca0cc3',
+    'name': 'd2uemrj0nk0b8u',
     'port': 5432,
 }
