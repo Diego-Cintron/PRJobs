35011493647e1c2a59b1938333177cdf249cd4ef
Diego-Cintron <diego.cintron1@upr.edu>
2024-04-08 11:35:38 -0400

    Fixing syntax

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
 const port = process.env.PORT || 3000;
-app.listen(port);
-
-console.log(`Server started on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
