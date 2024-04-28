45a9ef30d8d94227dfc4b09c3c1083a56c24c0ca
Finn111 <60770307+Finn111@users.noreply.github.com>
2024-04-28 12:35:10 -0400

    testing ads

--- src/About.js
+++ src/About.js
@@ -4,6 +4,11 @@ function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2843724221280103"
+        crossorigin="anonymous"
+      ></script>
 
       <p className="bottom-bar"> </p>
     </div>
