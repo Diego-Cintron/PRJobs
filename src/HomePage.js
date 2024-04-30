3a528cdbaf0a6b23466a0cb162eb82a97c7367c5
Finn111 <60770307+Finn111@users.noreply.github.com>
2024-04-30 19:01:38 -0400

    Testing ads

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,6 +4,11 @@ const HomePage = () => {
   return (
     <div className="homepage-block">
       <AllPostings />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2843724221280103"
+        crossorigin="anonymous"
+      ></script>
     </div>
   );
 };
