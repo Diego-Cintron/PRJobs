34cd7db26b6e3ab36858a69f69a6b1da61a265c1
Diego-Cintron <diego.cintron1@upr.edu>
2024-05-01 18:36:00 -0400

    Minor changes to ad sense style

--- src/Adsense.js
+++ src/Adsense.js
@@ -8,7 +8,7 @@ const AdSense = () => {
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "block" }}
+      style={{ display: "block", minWidth: "200px", minHeight: "200px" }}
       data-ad-client="ca-pub-2843724221280103"
       data-ad-slot="4216731899"
       data-ad-format="auto"
